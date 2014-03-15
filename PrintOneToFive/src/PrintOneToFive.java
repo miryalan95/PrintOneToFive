c6c889849808e2714609e8ed61a4d863cac71165
Naveen Kumar <miryalan95@gmail.com>
2014-03-15 17:32:04 -0500

    First Commit

---
+++ PrintOneToFive/src/PrintOneToFive.java
@@ -0,0 +1,24 @@
+
+public class PrintOneToFive {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++)
+			System.out.println(i);
+		int i=1;
+		while(i<=5)
+		{
+			System.out.println(i);
+			i++;
+		}
+i=1;
+do
+	
+{
+	System.out.println(i);
+	i++;
+	}
+while(i<=5);
+		
+	}
+
+}
